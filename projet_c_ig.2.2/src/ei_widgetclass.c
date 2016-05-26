f2a837a6538963c34638c6604fe1cf1e89c46752
Tont Buthod <tony.buthod@garcon@ensimag.grenoble-inp.fr>
2016-05-26 14:33:32 +0200

    Initialisation fichiers c pour éviter problèmes de fusion

---
+++ projet_c_ig.2.2/src/ei_widgetclass.c
@@ -0,0 +1,18 @@
+#include "ei_widgetclass.h"
+
+
+/* void   ei_widgetclass_register  (ei_widgetclass_t* widgetclass); */
+
+
+/* ei_widgetclass_t* ei_widgetclass_from_name (ei_widgetclass_name_t name); */
+
+
+/* void   ei_frame_register_class  (); */
+
+
+/* void   ei_button_register_class  (); */
+
+
+/* void   ei_toplevel_register_class  (); */
+
+
