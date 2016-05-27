ada570d0f3280a4b2219a102779625d8f3ab073f
Tont Buthod <tony.buthod@garcon@ensimag.grenoble-inp.fr>
2016-05-27 08:47:34 +0200

    Simplification Makefile + ei_app_run + création variables globales

--- projet_c_ig.2.2/src/ei_application.c
+++ projet_c_ig.2.2/src/ei_application.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+ei_surface_t main_window;
+ei_surface_t main_window_picking;
+ei_widget_t* root_widget=NULL;
 
 /* void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen); */
 void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen){
@@ -18,9 +21,23 @@ void ei_app_free(){
         /* Corps vide */
 }
 
+void ei_app_run_rec(ei_widget_t* w)
+{
+        if (w == NULL) {
+                return;
+        }
+        w->wclass->drawfunc(w, main_window, main_window_picking, w->parent->content_rect);
+        ei_widget_t* cour = w->children_head;
+        while (cour != NULL) {
+                ei_app_run_rec(cour);
+                cour = cour->next_sibling;
+        }
+        ei_app_run_rec(w->next_sibling);
+}
+
 /* void ei_app_run(); */
 void ei_app_run(){
-        getchar();
+        ei_app_run_rec(frame);
 }
 
 /* void ei_app_invalidate_rect(ei_rect_t* rect); */
