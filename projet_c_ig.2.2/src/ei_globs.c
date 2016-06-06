5266dd2d5d40baf87499f36e3c782934ddb7dffd
Tony Buthod <tony.buthod-garcon@ensimag.grenoble-inp.fr>
2016-06-06 18:03:59 +0200

    Enleve de l'optimisation d'affichage car plante

--- projet_c_ig.2.2/src/ei_globs.c
+++ projet_c_ig.2.2/src/ei_globs.c
@@ -10,6 +10,7 @@
 #include "ei_eventmanager.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 ei_rect_t union_rect( ei_rect_t rect1, ei_rect_t rect2){
 	ei_rect_t result;
@@ -19,19 +20,15 @@ ei_rect_t union_rect( ei_rect_t rect1, ei_rect_t rect2){
                 rect1 = rect2;
                 rect2 = tmp;
 	}
-	
-	int diffx=rect1.top_left.x-rect2.top_left.x;
-	int diffy=rect1.top_left.y-rect2.top_left.y;
 
-	if (diffy <=0){
-		result.top_left=rect1.top_left;
-		result.size.width=rect1.size.width + rect2.size.width - diffx;
-		result.size.height=rect1.size.height + rect2.size.height - diffy;
-	}else{
-		result.top_left.x=rect1.top_left.x;
-		result.top_left.y=rect1.top_left.y + diffy + rect2.top_left.y;
-		result.size.width=rect1.size.width + rect2.size.width - diffx;
-		result.size.height=rect1.size.height + rect2.size.height + diffy;
-	}
+	result.top_left.y=(rect1.size.height <= rect2.size.height)?rect1.size.height:rect2.size.height;
+	result.top_left.x=rect1.top_left.x ;
+
+	result.size.width=(rect1.size.width + rect1.top_left.x <= rect2.size.width + rect2.top_left.x)?
+		rect2.size.width + rect2.top_left.x:rect1.size.width + rect1.top_left.x;
+	result.size.height=(rect1.size.height + rect1.top_left.y <= rect2.size.height + rect2.top_left.y)?
+		rect2.size.height + rect2.top_left.y:rect1.size.height + rect1.top_left.y;
+
+
 	return result;
 }
