5627d3e2a4333a981e0f3273beaf1007b99b9bb4
Kylian VINCENT <kylian.vincent@ensimag.grenoble-inp.fr>
2016-06-08 09:56:59 +0200

    Ajout des fichiers pour la classe de widget entry

---
+++ projet_c_ig.2.2/src/ei_entry.c
@@ -0,0 +1,24 @@
+#include "ei_types.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/* --------------ENTRY-------------- */
+
+void ei_entry_configure(struct ei_widget_t *widget, ei_color_t* color, char **text, ei_font_t *text_font, ei_color_t *text_color, int *char_max){
+
+}
+
+/* void entry_geomnotifyfunc (struct ei_widget_t *widget, ei_rect_t rect); */
+
+/* void* entry_allocfunc (); */
+
+/* void entry_releasefunc (struct ei_widget_t* widget); */
+
+/* void entry_drawfunc(struct ei_widget_t* widget, */
+/* 		     ei_surface_t		surface, */
+/* 		     ei_surface_t		pick_surface, */
+/* 		     ei_rect_t*		clipper); */
+
+/* void entry_setdefaultsfunc(struct ei_widget_t* widget); */
+
+/* void ei_entry_register_class(); */
