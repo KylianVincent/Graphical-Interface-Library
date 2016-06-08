ef902fe10f187dfc76754c3efe4481f1329d397f
Kylian VINCENT <kylian.vincent@ensimag.grenoble-inp.fr>
2016-06-08 11:13:30 +0200

    Implémentation des fonctions spécifiques à la classe de widget "entry" - bugs possibles

--- projet_c_ig.2.2/include/ei_entry.h
+++ projet_c_ig.2.2/include/ei_entry.h
@@ -1,6 +1,8 @@
 
 /* Fonctions spécifique à la classe de widget entry */
-void ei_entry_configure(struct ei_widget_t *widget, ei_color_t* color, char **text, ei_font_t *text_font, ei_color_t *text_color, int *char_max);
+void ei_entry_configure(struct ei_widget_t *widget, ei_size_t *requested_size,
+                        ei_color_t* color, char **text, ei_font_t *text_font,
+                        ei_color_t *text_color, int *char_max);
 
 void entry_geomnotifyfunc (struct ei_widget_t *widget, ei_rect_t rect);
 
