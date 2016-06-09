140bcd08d6bc99e2bec3af2038bf869526d281e2
Kylian VINCENT <kylian.vincent@ensimag.grenoble-inp.fr>
2016-06-09 10:36:48 +0200

    Revert "ajout bureau"
    This reverts commit 3fd3f2cbde2a5e8c588b892fe46446fc07632f5d.

--- projet_c_ig.2.2/tests/frame_button.c
+++ projet_c_ig.2.2/tests/frame_button.c
@@ -15,44 +15,44 @@
  */
 int ei_main(int argc, char** argv)
 {
-	ei_size_t	screen_size		= {1500, 900};
+	ei_size_t	screen_size		= {600, 600};
 	ei_color_t	root_bgcol		= {0x52, 0x7f, 0xb4, 0xff};
-	ei_color_t      button_color		= {0x00, 0xAA, 0x00, 0xff};
+
 	ei_widget_t*	frame;
-	ei_size_t	frame_size		= {1500,35};
+	ei_size_t	frame_size		= {300,200};
 	int		frame_x			= 150;
 	int		frame_y			= 200;
-	ei_color_t	frame_color		= {0x00, 0x00, 0xAA, 0xff};
+	ei_color_t	frame_color		= {0x88, 0x88, 0x88, 0xff};
 	ei_relief_t	frame_relief		= ei_relief_raised;
-	int		frame_border_width	= 1;
+	int		frame_border_width	= 6;
 
-	/*arrière plan bureau*/
+	/* Create the application and change the color of the background. */
 	ei_app_create(&screen_size, EI_FALSE);
-        ei_surface_t img = hw_image_load("wallpaper.jpg", ei_app_root_surface());
+        ei_surface_t img = hw_image_load("test.jpg", ei_app_root_surface());
         ei_anchor_t anch_img = ei_anc_northwest;
         ei_rect_t *rect_img = calloc(1, sizeof(ei_rect_t));
+        char* text2 = "En bas";
         ei_anchor_t anch2 = ei_anc_south;
-        rect_img->top_left.x=0;rect_img->top_left.y=0; 
-        rect_img->size.width=1920; rect_img->size.height=1080;
-	ei_frame_configure(ei_app_root_widget(), NULL, &root_bgcol, NULL, NULL, NULL, NULL, NULL, &anch2, &img, &rect_img, &anch_img); 
+        rect_img->top_left.x=470;rect_img->top_left.y=230; 
+        rect_img->size.width=70; rect_img->size.height=70;
+	ei_frame_configure(ei_app_root_widget(), NULL, &root_bgcol, NULL, NULL, &text2, NULL, NULL, &anch2, NULL, NULL, NULL);
         
-	/* barre des taches */
+	/* Create, configure and place the frame on screen. */
 	frame = ei_widget_create("frame", ei_app_root_widget());
 	ei_frame_configure(frame, &frame_size, &frame_color,
 			    &frame_border_width, &frame_relief, NULL, NULL, NULL, NULL,
-			    NULL, NULL, NULL);
-        int x=0;
-        int y = 863;
-        float relx =0;
-        float rely =0;
+			    &img, &rect_img, &anch_img);
+        int x= -4;
+        int y = -4;
+        float relx = 1.0;
+        float rely = 1.0;
         ei_anchor_t anc_frame = ei_anc_southeast;
-	ei_place(frame, NULL, &x, &y, NULL, NULL, &relx, &rely, NULL, NULL );
-
+	ei_place(frame, &anc_frame, &x, &y, NULL, NULL, &relx, &rely, NULL, NULL );
         ei_widget_t* button;
         button = ei_widget_create("button", frame);
-        char* text = "Demarrer";
+        char* text = "Cliquez ici";
         ei_anchor_t anch = ei_anc_center;
-        ei_button_configure(button, NULL, &button_color, NULL, NULL, NULL, &text, NULL, NULL, &anch, NULL, NULL, NULL, NULL, NULL);
+        ei_button_configure(button, NULL, &root_bgcol, NULL, NULL, NULL, &text, NULL, NULL, &anch, NULL, NULL, NULL, NULL, NULL);
         ei_place(button, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 	/* Run the application's main loop. */
 	ei_app_run();
